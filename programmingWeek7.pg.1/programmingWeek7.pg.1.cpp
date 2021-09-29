dc6bfd2fce7840c2653abe233572589fd67aa8e5
User <User@SC-202108212122>
2021-09-29 23:44:58 +0700

    Add project files.

---
+++ programmingWeek7.pg.1/programmingWeek7.pg.1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int height;
+    int height_sum = 0;
+    float average;
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << "Enter the height for " << i << " student : ";
+        cin >> height;
+        height_sum = height_sum + height;
+    }
+    average = height_sum / 10.0;
+    cout << "Average height is : " << average;
+
+    return 0;
+}
